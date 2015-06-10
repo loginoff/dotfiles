b6754f0c519e45348db392e26ae9ad53da825d24
Martin Loginov <martin.loginov@ut.ee>
2015-06-11 00:58:09 +0300

    Added a script to automatically generate symlinks into home

---
+++ link.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+BACKUP_DIR=~/.dotfiles_backup
+if [ -e $BACKUP_DIR ]
+then
+    echo "$BACKUP_DIR already exists"
+else
+    mkdir -v $BACKUP_DIR
+fi
+
+FILES=".bashrc .vimrc .zshrc"
+for file in $FILES
+do
+    if [ -f ~/$file ]
+    then
+        mv -v ~/$file $BACKUP_DIR
+    fi
+    ln -s $BASEDIR/$file ~/$file
+done
